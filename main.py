6e74823ad684609f2ac58dcdd7ec43ad907b2bc4
eRiKa4530 <omurbekovaerika@gmail.com>
2023-01-26 12:13:11 +0600

    new test

---
+++ main.py
@@ -0,0 +1,32 @@
+'''Работа с файлами'''
+# file = open('text1.txt','r')
+# # file.write('Hello')
+# file.writelines('Hello\n')
+# file.write('Word')
+# file.close()
+# file.write('hggkk')
+# f = file.read(2)
+# print(file.close())
+# file.close()
+# # print(file.readline())
+# print(file.readlines())
+# with open('test1.txt') as file:
+#     print(file.read())
+    
+
+
+'''JSON'''
+# сериализация(c pyhotn B json) -> dump, dumps
+#Десериализация (с json B python) -> load, loads
+# import json
+# d = {'hello':True,'afe':2,'name':None}
+# json_obj = json.dumps(d)
+# print(json_obj)
+# pyhton_obj = json.loads(json_obj)
+# print(pyhton_obj)
+# import json
+# with open('data.json','r') as file:
+#     # python_obj = json.loads(file.read())
+#     python_obj = json.load(file)
+#     print(python_obj)
+'''OOP'''
